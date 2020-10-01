adee1867b80b954afac5c9168fb810182d043968
Daniel19902 <60512110+Daniel19902@users.noreply.github.com>
2020-09-30 23:18:56 -0500

    Complete

--- src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
+++ src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
@@ -21,6 +21,15 @@ package edu.eci.cvds.samples.services.client;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ClienteMapper;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ItemMapper;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ItemRentadoMapper;
+import edu.eci.cvds.samples.entities.Item;
+import edu.eci.cvds.samples.entities.TipoItem;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -57,25 +66,39 @@ public class MyBatisExample {
      * @param args
      * @throws SQLException 
      */
-    public static void main(String args[]) throws SQLException {
+    public static void main(String args[]) throws SQLException, ParseException {
         SqlSessionFactory sessionfact = getSqlSessionFactory();
-
         SqlSession sqlss = sessionfact.openSession();
+        ClienteMapper cm = sqlss.getMapper(ClienteMapper.class);
+        ItemMapper ir = sqlss.getMapper(ItemMapper.class);
+        ItemRentadoMapper im = sqlss.getMapper(ItemRentadoMapper.class);
 
-        
-        //Crear el mapper y usarlo: 
-        //ClienteMapper cm=sqlss.getMapper(ClienteMapper.class)
-        //cm...
-        
-        
-        
+        System.out.println("Consultar Clientes");
+
+        System.out.println(cm.consultarClientes());
+
+        System.out.println("Consultar cliente");
+        System.out.println(cm.consultarCliente(2));
+
+        cm.agregarItemRentadoACliente(1,2 ,new SimpleDateFormat("yyyy/MM/dd").parse("2022/09/28"),new SimpleDateFormat("yyyy/MM/dd").parse("2022/10/28"));
+        System.out.println("item rentado agregado");
+
+        System.out.println("Consultar item agregados a clientes");
+        System.out.println(cm.consultarCliente(1));
+
+        TipoItem tipoItem = new TipoItem(1,"Batman");
+        ir.insertarItem(new Item(tipoItem,97804578,"Batman cvds","batman prueba",new SimpleDateFormat("yyy-MM-dd").parse("2020-10-01"),80,"cvd","accion"));
+        System.out.println("Item insertado");
+
+        System.out.println("consultar item 45612");
+        System.out.println(ir.consultarItem(97804578));
+
+        System.out.println("consultar items");
+        System.out.println(ir.consultarItems());
+
         sqlss.commit();
-        
-        
         sqlss.close();
 
-        
-        
     }
 
 
